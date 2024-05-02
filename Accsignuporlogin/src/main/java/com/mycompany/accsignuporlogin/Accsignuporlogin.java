24e10c0eb7e73559fc8dd768efdcfc17cc3ee3cd
PBvergara <166792727+PBvergara@users.noreply.github.com>
2024-05-02 18:19:39 +0800

    Add files via upload

---
+++ Accsignuporlogin/src/main/java/com/mycompany/accsignuporlogin/Accsignuporlogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.accsignuporlogin;
+
+/**
+ *
+ * @author PAUL BRYAN
+ */
+public class Accsignuporlogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
